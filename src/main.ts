e0c2ef3ea5b267c9e94c6ccba9fb9dd957c7e09f
grenzk <74033991+grenzk@users.noreply.github.com>
2024-06-15 09:40:59 +0800

    Install and set up primevue

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from '@/App.vue'
 import { router } from '@/router'
 
 import '@/assets/scss/styles.scss'
+import 'primevue/resources/themes/aura-light-indigo/theme.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  ripple: true
+})
 
 app.mount('#app')
