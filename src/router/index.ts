760266d8194f9fc99cf21c45fd0acbcc9ac80833
grenzk <74033991+grenzk@users.noreply.github.com>
2024-06-17 18:48:13 +0800

    Define initial HTML structure for new-feedback page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
+import NewFeedbackPage from '@/views/NewFeedbackPage.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -8,6 +9,11 @@ export const router = createRouter({
       path: '/',
       name: 'home',
       component: HomePage
+    },
+    {
+      path: '/feedback/new',
+      name: 'new feedback',
+      component: NewFeedbackPage
     }
   ]
 })
---
+++ src/views/NewFeedbackPage.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext'
+import Dropdown from 'primevue/dropdown'
+import Textarea from 'primevue/textarea'
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <main>
+    <RouterLink to="/">
+      <img src="@/assets/images/shared/icon-arrow-left.svg" alt="" />
+      <span>Go Back</span>
+    </RouterLink>
+
+    <form>
+      <h1>Create New Feedback</h1>
+
+      <div>
+        <label>Feedback Title</label>
+        <p>Add a short, descriptive headline</p>
+        <InputText />
+        <small>Input error</small>
+      </div>
+
+      <div>
+        <label>Category</label>
+        <p>Choose a category for your feedback</p>
+        <Dropdown />
+        <small>Input error</small>
+      </div>
+
+      <div v-if="false">
+        <label>Update Status</label>
+        <p>Change feature state</p>
+        <Dropdown />
+        <small>Input error</small>
+      </div>
+
+      <div>
+        <label>Feedback Detail</label>
+        <p>Include any specific comments on what should be improved, added, etc.</p>
+        <Textarea auto-resize />
+        <small>Input error</small>
+      </div>
+
+      <Button label="Add feedback" />
+      <RouterLink to="/">
+        <Button label="Cancel" />
+      </RouterLink>
+      <Button v-if="false" label="Delete" severity="danger" />
+    </form>
+  </main>
+</template>
+
+<style lang="scss"></style>
