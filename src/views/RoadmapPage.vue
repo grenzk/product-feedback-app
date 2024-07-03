0583153c034319b082bea77c1c89fee8b99bdb0d
grenzk <74033991+grenzk@users.noreply.github.com>
2024-07-03 11:33:11 +0800

    Add style to roadmap page

--- src/views/RoadmapPage.vue
+++ src/views/RoadmapPage.vue
@@ -6,50 +6,108 @@ import NewFeedbackLink from '@/components/NewFeedbackLink.vue'
 </script>
 
 <template>
-  <header>
-    <RouterLink to="/">
-      <img src="@/assets/images/shared/icon-arrow-left.svg" alt="" />
-      <span>Go Back</span>
-    </RouterLink>
-    <h1>Roadmap</h1>
+  <header id="roadmap-header" class="l-flex">
+    <div>
+      <RouterLink to="/" class="back-link l-flex">
+        <img src="@/assets/images/shared/icon-arrow-left.svg" alt="" class="icon" />
+        <span>Go Back</span>
+      </RouterLink>
+      <h1>Roadmap</h1>
+    </div>
     <NewFeedbackLink />
   </header>
 
   <nav>
-    <ul>
-      <li>Planned(2)</li>
-      <li>In-Progress(3)</li>
-      <li>Live(1)</li>
+    <ul class="l-flex">
+      <li>Planned (2)</li>
+      <li>In-Progress (3)</li>
+      <li>Live (1)</li>
     </ul>
   </nav>
 
-  <h2>In-Progress(3)</h2>
-  <p>Features</p>
-
-  <div>
-    <div>
-      <div>
-        <span></span>
-        In Progress
-      </div>
-      <h3>One-click portfolio generation</h3>
-
-      <p>Add ability to create professional looking portfolio from profile.</p>
-      <Tag value="Feature" />
+  <main id="main">
+    <div class="text-group l-container">
+      <h2>In-Progress (3)</h2>
+      <p>Features currently being developed.</p>
     </div>
 
-    <div>
-      <div>
-        <img src="@/assets/images/shared/icon-arrow-up.svg" alt="" />
-        <span>112</span>
+    <section class="feedback l-container">
+      <div class="column l-flex">
+        <div>
+          <h3 class="feedback-title">Add tags for solutions</h3>
+          <p class="feedback-description">
+            Easier to search for solutions based on a specific stack.
+          </p>
+          <Tag value="Enhancement"></Tag>
+        </div>
+
+        <div class="upvote-counter">
+          <img src="@/assets/images/shared/icon-arrow-up.svg" alt="" />
+          <span>112</span>
+        </div>
       </div>
 
-      <div>
+      <div class="comment-counter">
         <img src="@/assets/images/shared/icon-comments.svg" alt="" />
         <span>2</span>
       </div>
-    </div>
-  </div>
+    </section>
+  </main>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+#roadmap-header {
+  background-color: var(--color-primary-indigo-dark-1);
+  padding: 1.625rem 1.5rem;
+  justify-content: space-between;
+  align-items: center;
+  row-gap: 3px;
+
+  .back-link {
+    align-items: center;
+    column-gap: 1rem;
+    text-decoration: none;
+    font-weight: 700;
+    font-size: var(--font-size-xxs);
+    color: var(--color-neutral-white-1);
+
+    .icon {
+      filter: invert(1%) sepia(1%) saturate(1%) hue-rotate(1deg) brightness(1000%) contrast(100%);
+    }
+  }
+
+  h1 {
+    font-size: var(--font-size-l);
+    color: var(--color-neutral-white-1);
+    letter-spacing: -0.25px;
+  }
+}
+
+nav {
+  padding: 1.25rem 2.5rem 1.25rem 1.5rem;
+  border-bottom: 1px solid hsla(231, 20%, 63%, 0.25);
+  margin-bottom: 1rem;
+
+  ul {
+    justify-content: space-between;
+
+    li {
+      color: var(--color-primary-indigo-dark-2);
+      font-weight: 700;
+      letter-spacing: -0.18px;
+      text-align: center;
+    }
+  }
+}
+
+.text-group {
+  margin-bottom: 1rem;
+
+  h2 {
+    letter-spacing: -0.25px;
+  }
+}
+
+.card {
+}
+</style>
