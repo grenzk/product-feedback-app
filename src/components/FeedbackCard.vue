738039ae36086a933ae2faa424cff51c106cda45
grenzk <74033991+grenzk@users.noreply.github.com>
2024-07-07 16:54:23 +0800

    Componentize feedback

---
+++ src/components/FeedbackCard.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import Tag from 'primevue/tag'
+import ContentCard from './ContentCard.vue'
+</script>
+
+<template>
+  <ContentCard class="feedback | l-container">
+    <div class="column | l-flex">
+      <div>
+        <h3>Add tags for solutions</h3>
+        <p>Easier to search for solutions based on a specific stack.</p>
+        <Tag value="Enhancement"></Tag>
+      </div>
+
+      <div class="upvote-counter | text-bold">
+        <img src="@/assets/images/shared/icon-arrow-up.svg" alt="" aria-hidden="true" />
+        <span>112</span>
+      </div>
+    </div>
+
+    <div class="comment-counter | text-bold">
+      <img src="@/assets/images/shared/icon-comments.svg" alt="" aria-hidden="true" />
+      <span>2</span>
+    </div>
+  </ContentCard>
+</template>
+
+<style lang="scss">
+.feedback {
+  --counter-padding: 0.375rem;
+
+  position: relative;
+  padding: 1.5rem;
+
+  .column {
+    flex-direction: column;
+    row-gap: 1rem;
+
+    h3 {
+      font-size: inherit;
+      letter-spacing: -0.18px;
+    }
+
+    p {
+      margin: 0.563rem 0 0.5rem 0;
+    }
+
+    .upvote-counter {
+      display: inline-flex;
+      background-color: var(--color-neutral-white-4);
+      color: var(--color-primary-indigo-dark-2);
+      letter-spacing: -0.18px;
+      align-items: center;
+      align-self: flex-start;
+      column-gap: 10px;
+      padding: var(--counter-padding) 0.813rem var(--counter-padding) 1rem;
+      border-radius: var(--border-radius-m);
+    }
+  }
+
+  .comment-counter {
+    display: inline-flex;
+    position: absolute;
+    right: 2rem;
+    bottom: 1.5rem;
+    color: var(--color-primary-indigo-dark-2);
+    letter-spacing: -0.18px;
+    align-items: center;
+    align-self: flex-end;
+    column-gap: 8px;
+    margin-bottom: var(--counter-padding);
+  }
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { uiStore } from '@/stores/ui'
 
-import Tag from 'primevue/tag'
 import SidebarMenu from '@/components/SidebarMenu.vue'
 import ContentCard from '@/components/ContentCard.vue'
+import FeedbackCard from '@/components/FeedbackCard.vue'
 import FeedbackControls from '@/components/FeedbackControls.vue'
 import NewFeedbackLink from '@/components/NewFeedbackLink.vue'
 </script>
@@ -13,25 +13,7 @@ import NewFeedbackLink from '@/components/NewFeedbackLink.vue'
   <FeedbackControls />
 
   <main class="home | l-flex">
-    <ContentCard class="feedback | l-container">
-      <div class="column | l-flex">
-        <div>
-          <h3>Add tags for solutions</h3>
-          <p>Easier to search for solutions based on a specific stack.</p>
-          <Tag value="Enhancement"></Tag>
-        </div>
-
-        <div class="upvote-counter | text-bold">
-          <img src="@/assets/images/shared/icon-arrow-up.svg" alt="" aria-hidden="true" />
-          <span>112</span>
-        </div>
-      </div>
-
-      <div class="comment-counter | text-bold">
-        <img src="@/assets/images/shared/icon-comments.svg" alt="" aria-hidden="true" />
-        <span>2</span>
-      </div>
-    </ContentCard>
+    <FeedbackCard />
 
     <ContentCard v-if="false" class="empty-state | l-container">
       <img src="@/assets/images/suggestions/illustration-empty.svg" alt="" aria-hidden="true" />
@@ -54,52 +36,6 @@ import NewFeedbackLink from '@/components/NewFeedbackLink.vue'
   padding-top: 1rem;
   flex-direction: column;
   row-gap: 1rem;
-
-  .feedback {
-    --counter-padding: 0.375rem;
-
-    position: relative;
-    padding: 1.5rem;
-
-    .column {
-      flex-direction: column;
-      row-gap: 1rem;
-
-      h3 {
-        font-size: inherit;
-        letter-spacing: -0.18px;
-      }
-
-      p {
-        margin: 0.563rem 0 0.5rem 0;
-      }
-
-      .upvote-counter {
-        display: inline-flex;
-        background-color: var(--color-neutral-white-4);
-        color: var(--color-primary-indigo-dark-2);
-        letter-spacing: -0.18px;
-        align-items: center;
-        align-self: flex-start;
-        column-gap: 10px;
-        padding: var(--counter-padding) 0.813rem var(--counter-padding) 1rem;
-        border-radius: var(--border-radius-m);
-      }
-    }
-
-    .comment-counter {
-      display: inline-flex;
-      position: absolute;
-      right: 2rem;
-      bottom: 1.5rem;
-      color: var(--color-primary-indigo-dark-2);
-      letter-spacing: -0.18px;
-      align-items: center;
-      align-self: flex-end;
-      column-gap: 8px;
-      margin-bottom: var(--counter-padding);
-    }
-  }
 
   .empty-state {
     padding: 4.75rem 1.5rem;
