c7faf3eaa477d1d22badf622f05b0e01658f4710
grenzk <74033991+grenzk@users.noreply.github.com>
2024-07-26 15:50:57 +0800

    Compnentize link

---
+++ src/components/BackLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <RouterLink class="back-link l-flex" to="/">
+    <img src="@/assets/images/shared/icon-arrow-left.svg" alt="" aria-hidden="true" />
+    <span class="text-bold">Go Back</span>
+  </RouterLink>
+</template>
+
+<style lang="scss">
+.back-link {
+  align-items: center;
+  column-gap: 1rem;
+  text-decoration: none;
+  color: var(--color-neutral-gray);
+}
+</style>
