f9fad8435e6fa1ff9340d616b427933d52a2e1fa
grenzk <74033991+grenzk@users.noreply.github.com>
2024-10-09 21:07:02 +0800

    feat: add styles to dropdown panel

--- src/components/FeedbackControls.vue
+++ src/components/FeedbackControls.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import Button from 'primevue/button'
+import Dropdown from 'primevue/dropdown'
 import NewFeedbackLink from '@/components/NewFeedbackLink.vue'
+
+const sortOptions = ref(['Most Upvotes', 'Least Upvotes', 'Most Comments', 'Least Comments'])
+const selectedOption = ref('Most Upvotes')
 </script>
 
 <template>
@@ -11,18 +14,25 @@ import NewFeedbackLink from '@/components/NewFeedbackLink.vue'
     </div>
 
     <div class="column">
-      <Button text>
-        <span>Sort by :</span>
-        <span class="text-bold">
-          Most Upvotes
-          <img
-            src="@/assets/images/shared/icon-arrow-down.svg"
-            alt=""
-            class="icon"
-            aria-hidden="true"
-          />
-        </span>
-      </Button>
+      <Dropdown v-model="selectedOption" :options="sortOptions">
+        <template #value="slotProps">
+          <div v-if="slotProps.value" class="dropdown-value | l-flex">
+            <span>Sort by : </span>
+            <span class="text-bold">{{ slotProps.value }}</span>
+          </div>
+        </template>
+        <template #option="slotProps">
+          <div class="dropdown-option | l-flex">
+            <span>{{ slotProps.option }}</span>
+            <img
+              v-if="selectedOption === slotProps.option"
+              src="@/assets/images/shared/icon-check.svg"
+              alt=""
+              aria-hidden="true"
+            />
+          </div>
+        </template>
+      </Dropdown>
     </div>
     <NewFeedbackLink />
   </section>
@@ -47,20 +57,27 @@ import NewFeedbackLink from '@/components/NewFeedbackLink.vue'
   }
 
   .column:nth-child(2) {
-    .p-button {
-      color: var(--color-neutral-white-4);
-      padding: 0;
-      cursor: pointer;
-      column-gap: 5px;
-
-      .icon {
-        display: inline;
-        margin-left: 3px;
-        filter: invert(1%) sepia(1%) saturate(1%) hue-rotate(1deg) brightness(1000%) contrast(100%);
-      }
+    .p-dropdown {
+      background: transparent;
+      column-gap: 0.5rem;
+      outline: 0;
 
       &:hover {
         opacity: 75%;
+      }
+
+      .p-dropdown-label {
+        color: var(--color-neutral-white-4);
+        padding: 0;
+      }
+
+      .p-dropdown-trigger {
+        width: 0.7rem;
+        filter: invert(1%) sepia(1%) saturate(1%) hue-rotate(1deg) brightness(1000%) contrast(100%);
+      }
+
+      .dropdown-value {
+        column-gap: 5px;
       }
     }
   }
@@ -71,8 +88,9 @@ import NewFeedbackLink from '@/components/NewFeedbackLink.vue'
 }
 
 @media screen and (max-width: 350px) {
-  .column:nth-child(2) > .p-button {
+  .column:nth-child(2) > .p-dropdown > .p-dropdown-label > .dropdown-value {
     flex-direction: column;
+    text-align: center;
   }
 }
 
@@ -87,12 +105,6 @@ import NewFeedbackLink from '@/components/NewFeedbackLink.vue'
 
       h2 {
         padding-bottom: 2px;
-      }
-    }
-
-    .column:nth-child(2) {
-      .p-button {
-        font-size: var(--font-size-xs);
       }
     }
   }
