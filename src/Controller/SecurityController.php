3e48b7b2f25db16b37f73482810a90d5f5ecf65f
grenzk <74033991+grenzk@users.noreply.github.com>
2024-11-11 18:55:02 +0800

    feat: add login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use ApiPlatform\Metadata\IriConverterInterface;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'app_login', methods: ['POST'])]
+    public function login(IriConverterInterface $iriConverter, #[CurrentUser] User $user = null): Response
+    {
+        if (!$user) {
+            return $this->json([
+                'error' => 'Invalid login request: Set Content-Type to "application/json"'
+            ], 401);
+        }
+
+        return new Response(null, 204, [
+            "Location" => $iriConverter->getIriFromResource($user)
+        ]);
+    }
+}
