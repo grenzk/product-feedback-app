437e5bec4003c59242e393749f2cd6084618844e
grenzk <74033991+grenzk@users.noreply.github.com>
2024-11-18 16:21:23 +0800

    refactor: rewrite routing for vue

--- src/Controller/VueController.php
+++ src/Controller/VueController.php
@@ -8,13 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class VueController extends AbstractController
 {
-    #[Route('/', name: 'client_home', methods: ['GET'])]
-    public function home(): Response
-    {
-        return $this->render('base.html.twig');
-    }
-
-    #[Route('/{path}', name: 'client_route', requirements: ['path' => '.+'], methods: ['GET'])]
+    #[Route('/{path}', requirements: ['path' => '.+'],  defaults: ['path' => null], methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('base.html.twig');
