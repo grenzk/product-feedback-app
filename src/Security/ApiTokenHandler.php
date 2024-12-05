6464e4699b6ae93885313f8ad1a9a03fa2766b88
grenzk <74033991+grenzk@users.noreply.github.com>
2024-12-05 20:55:00 +0800

    chore: format file

--- src/Security/ApiTokenHandler.php
+++ src/Security/ApiTokenHandler.php
@@ -10,22 +10,22 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 
 class ApiTokenHandler implements AccessTokenHandlerInterface
 {
-  public function __construct(private ApiTokenRepository $apiTokenRepository) {}
+    public function __construct(private ApiTokenRepository $apiTokenRepository) {}
 
-  public function getUserBadgeFrom(#[\SensitiveParameter] string $accessToken): UserBadge
-  {
-    $token = $this->apiTokenRepository->findOneBy(['token' => $accessToken]);
+    public function getUserBadgeFrom(#[\SensitiveParameter] string $accessToken): UserBadge
+    {
+        $token = $this->apiTokenRepository->findOneBy(['token' => $accessToken]);
 
-    if (!$token) {
-      throw new BadCredentialsException();
+        if (!$token) {
+            throw new BadCredentialsException();
+        }
+
+        if (!$token->isValid()) {
+            throw new CustomUserMessageAuthenticationException('Token expired');
+        }
+
+        $token->getOwnedBy()->markAsTokenAuthenticated($token->getScopes());
+
+        return new UserBadge($token->getOwnedBy()->getUserIdentifier());
     }
-
-    if (!$token->isValid()) {
-      throw new CustomUserMessageAuthenticationException('Token expired');
-    }
-
-    $token->getOwnedBy()->markAsTokenAuthenticated($token->getScopes());
-
-    return new UserBadge($token->getOwnedBy()->getUserIdentifier());
-  }
 }
