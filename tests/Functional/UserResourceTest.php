e04600a1cd7683462b2022c62c4db10acf79cd8d
grenzk <74033991+grenzk@users.noreply.github.com>
2024-12-12 18:09:50 +0800

    feat: add tests for user entity

---
+++ tests/Functional/UserResourceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Zenstruck\Browser\HttpOptions;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class UserResourceTest extends ApiTestCase
+{
+    use ResetDatabase;
+    use Factories;
+
+    public function testPostToCreateUser(): void
+    {
+        $this->browser()
+            ->post('/api/users', HttpOptions::json([
+                'email' => 'john.doe@example.com',
+                'username' => 'johndoe',
+                'password' => 'foobar'
+            ]))
+            ->assertStatus(201)
+            ->post('/login', HttpOptions::json([
+                'email' => 'john.doe@example.com',
+                'password' => 'foobar'
+            ]))
+            ->assertSuccessful()
+        ;
+    }
+}
