cecda1cf0fe9a456ac7888b96cbed3c9b2501afb
grenzk <74033991+grenzk@users.noreply.github.com>
2025-01-13 15:50:39 +0800

    fix: prevent duplicate upvotes

---
+++ migrations/Version20250112072905.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250112072905 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_68AB8766D249A8875E70BCD7 ON upvote (feedback_id, owned_by_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX UNIQ_68AB8766D249A8875E70BCD7');
+    }
+}
