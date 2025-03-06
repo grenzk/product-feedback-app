7c3eb7c901cb993d9d31280d2e4bd56fb698bff8
grenzk <74033991+grenzk@users.noreply.github.com>
2025-03-06 18:25:46 +0800

    chore: add SSL certificate handling for CockroachDB connection

--- scripts/00-symfony-deploy.sh
+++ scripts/00-symfony-deploy.sh
@@ -5,6 +5,8 @@ composer install --no-dev --optimize-autoloader
 echo "Caching config..."
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
 
+echo $SSL_ROOT_CERT | base64 --decode > '/tmp/root.crt'
+
 echo "Creating database if does not exists..."
 APP_ENV=prod APP_DEBUG=0 php bin/console doctrine:database:create --if-not-exists
 
