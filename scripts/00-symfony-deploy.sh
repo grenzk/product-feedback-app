866263ca6d374e6ed1cf421a1d5c170200e2a0fc
grenzk <74033991+grenzk@users.noreply.github.com>
2025-03-08 10:51:56 +0800

    build: build: update deployment script with database setup

--- scripts/00-symfony-deploy.sh
+++ scripts/00-symfony-deploy.sh
@@ -4,3 +4,9 @@ composer install --no-dev --optimize-autoloader
 
 echo "Caching config..."
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
+
+echo "Creating database..."
+APP_ENV=prod APP_DEBUG=0 php bin/console doctrine:database:create --if-not-exists
+
+echo "Migrating database..."
+APP_ENV=prod APP_DEBUG=0 php bin/console doctrine:migrations:migrate --no-interaction
