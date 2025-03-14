d98166632eac81a5957e19ed448b49d607e08ae2
grenzk <74033991+grenzk@users.noreply.github.com>
2025-03-14 14:58:17 +0800

    chore: remove setting env variables in deploy script

--- scripts/00-symfony-deploy.sh
+++ scripts/00-symfony-deploy.sh
@@ -3,16 +3,16 @@ echo "Running composer"
 composer install --no-dev --optimize-autoloader
 
 echo "Caching config..."
-APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
+php bin/console cache:clear
 
 echo "Creating database..."
-APP_ENV=prod APP_DEBUG=0 php bin/console doctrine:database:create --if-not-exists
+php bin/console doctrine:database:create --if-not-exists
 
 echo "Migrating database..."
-APP_ENV=prod APP_DEBUG=0 php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console doctrine:migrations:migrate --no-interaction
 
 echo "Loading sample data..."
-APP_ENV=prod APP_DEBUG=0 php bin/console app:create-users
-APP_ENV=prod APP_DEBUG=0 php bin/console app:create-feedback
-APP_ENV=prod APP_DEBUG=0 php bin/console app:create-comments
-APP_ENV=prod APP_DEBUG=0 php bin/console app:create-upvotes
+php bin/console app:create-users
+php bin/console app:create-feedback
+php bin/console app:create-comments
+php bin/console app:create-upvotes
